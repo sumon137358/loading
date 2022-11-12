bd09e57a0bf32059335af30f72dc01ab414b666f
sumon <sumon137358@gmail.com>
2022-11-12 23:33:02 +0600

    some problem solve

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
+document.getElementById("searchBtn").addEventListener("click", function () {
+  const loadingBtn = document.querySelector("#loading");
+  loadingBtn.style.display = "block";
 
-
-document.getElementById("searchBtn").addEventListener("click",function(){
-    
-    const loadingBtn =document.querySelector(".loading-control");
-    loadingBtn.style.display ="block";
-    
-    var dots = window.setInterval( function() {
-        var wait = document.getElementById("wait");
-        if ( wait.innerHTML.length > 3 ) 
-            wait.innerHTML = "";
-        else 
-            wait.innerHTML += ".";
-        }, 1100);
-})
-
-
-
-
-
-
-
-
+  var dots = window.setInterval(function () {
+    var wait = document.getElementById("wait");
+    if (wait.innerHTML.length > 3) wait.innerHTML = "";
+    else wait.innerHTML += ".";
+  }, 1000);
+});
